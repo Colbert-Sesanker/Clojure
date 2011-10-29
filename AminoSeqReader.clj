0119255942dfdde2ef6564527a86fa6c07127d2a
Sesanker <sesanker0@gmail.com>
2011-10-29 10:23:46 -0400

    Snipits of code

---
+++ AminoSeqReader.clj
@@ -0,0 +1,48 @@
+(ns machine
+  (:use clojure.contrib.combinatorics)) 
+
+(zipmap (sort (map read-string
+ (map #(apply str %) (cartesian-product 
+       (repeat 2 ("R" "H" "K" "D" "E" "S" "T" "N" "Q" "C" "U" "G" "P" "A" "V" "I" "L" "M" "F" "Y" "W") )
+              )))) 
+(range 0 441 1)) ;;makes indexes fom 0 to 440
+
+
+(cartesian-product 
+       (repeat 2 ["R" "H" "K" "D" "E" "S" "T" "N" "Q" "C" "U" "G" "P" "A" "V" "I" "L" "M" "F" "Y" "W"] )
+              )
+
+(clojure.core/refer 'clojure.core)
+(ns machine
+  (:use (java.io FileReader BufferedReader))         
+  (:use (clojure.contrib.combinatorics) )     
+  (:use (cern.colt.matrix.tfloat.impl.SparseFloatMatrix1D))
+  (:use (cern.jet.math.tfloat FloatFunctions))
+  (:use (edu.emory.mathcs.utils ConcurrencyUtils)))
+
+
+(defn n-tuple-list [n list] "Integer- > TupleList -> List" 
+ (map #(nth % n) list))
+
+(defn stringseq [a b] "List -> List -> StringList"
+ (map str a  b))
+
+(defn stringseq-tuple [tuple-list] "TupleList -> StringList"
+ (map str (map first tuple-list) (map second tuple-list)))
+
+(def protein-neighbors 
+ (let [ proteins (repeat 2 ["R" "H" "K" "D" "E" "S" "T" "N" "Q" "C" "U" "G" "P" "A" "V" "I" "L" "M" "F" "Y" "W"])]
+	(cartesian-product (first proteins) (second proteins))))
+
+(def protein-neighborhood (let [ s (stringseq-tuple protein-neighbors)] 
+  (zipmap (into (stringseq s (repeat "1")) (stringseq s (repeat "2"))) 
+          (iterate inc 1))))
+
+(defn get-protein-neighbor-index [protein-neighbor] "String -> Int"
+	(protein-neighborhood protein-neighbor))
+
+
+
+
+
+
