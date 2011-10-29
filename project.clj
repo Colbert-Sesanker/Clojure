0baff17163183e716b86d0341a8cfa736ea521aa
Sesanker <sesanker0@gmail.com>
2011-10-28 20:01:01 -0400

    project file addedproject.clj

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject AminoSeqReader "0.0.1"
+:description "Application to predict gaps in Protein sequences"
+:dependencies [[org.clojure/clojure "1.2.1"]
+[org.clojure/clojure-contrib "1.2.0"]
+[commons-lang "2.3"]
+[incanter "1.2.3-SNAPSHOT"]])
+
+
+
