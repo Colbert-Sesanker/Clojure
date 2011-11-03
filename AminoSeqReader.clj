38858c15608eec1584c8624f1d8b7ecdb7b0489b
Sesanker <sesanker0@gmail.com>
2011-11-03 00:19:16 -0300

    Test using Legacy Pegasos algorithm.

--- AminoSeqReader.clj
+++ AminoSeqReader.clj
@@ -20,6 +20,30 @@
 (defn stringseq-tuple [tuple-list] "TupleList -> StringList"
  (map str (map first tuple-list) (map second tuple-list)))
 
+;; Mathematical functions from Mark Reid: http://mark.reid.name/sap/online-learning-in-clojure.html
+;; Will replace with incanter libraries
+(defn add
+	"Returns the sparse sum of two sparse vectors x y"
+	[x y] (merge-with + x y))
+
+(defn inner
+	"Computes the inner product of the sparse vectors (hashes) x and y"
+	[x y] (reduce + (map #(* (get x % 0) (get y % 0)) (keys y))))
+
+(defn norm
+	"Returns the l_2 norm of the (sparse) vector v"
+	[v] (Math/sqrt (inner v v)))
+
+(defn scale
+	"Returns the scalar product of the sparse vector v by the scalar a"
+	[a v] (zipmap (keys v) (map * (vals v) (repeat a))))
+
+(defn project
+	"Returns the projection of a parameter vector w onto the ball of radius r"
+	[w r] (scale (min (/ r (norm w)) 1) w))
+
+;;
+
 ;;
 ;;Devise scheme to transform FASTA peptide sequences into a training set that can train an SVM to fill gaps
 ;;in peptide sequences
@@ -35,11 +59,11 @@
 (defn get-protein-neighbor-index [protein-neighbor] "String -> Int"
 	(protein-neighborhood protein-neighbor))
 
-(defn target-neighbors [string]
+(defn target-neighbors [string] ;; returns neighborhoods about desired amino acid
  (let [ matches (re-seq #"..[A].." string) ]
     matches))
 
-(defn error-neighbors [string]
+(defn error-neighbors [string] ;;returns neighborhoods about undesired amino acid
  (let [ matches (re-seq #"..[S].." string) ]
     matches))
 
@@ -77,7 +101,62 @@ IENY")
 (def examples ;; examples used for training
  (interleave (map pos-example target-vectors) (map neg-example error-vectors)))
 
+;; Legacy Pegasos algorythym described by Mark Reid http://mark.reid.name/sap/online-learning-in-clojure.html
 
+(defn hinge-loss
+	"Returns the hinge loss of the weight vector w on the given example"
+	[w example] (max 0 (- 1 (* (:y example) (inner w (:x example))))))
+	
+(defn correct
+	"Returns a corrected version of the weight vector w"
+	[w example t lambda]
+	(let [x   (:x example)
+		  y   (:y example)
+		  w1  (scale (- 1 (/ 1 t)) w)
+		  eta (/ 1 (* lambda t))
+		  r   (/ 1 (Math/sqrt lambda))]
+		(project (add w1 (scale (* eta y) x)) r)))
 
+(defn report
+	"Prints some statistics about the given model at the specified interval"
+	[model interval]
+	(if (zero? (mod (:step model) interval))
+		(let [t      (:step model)
+		      size   (count (keys (:w model)))
+		      errors (:errors model) ]
+			(println "Step:" t 
+				 "\t Features in w =" size 
+				 "\t Errors =" errors 
+				 "\t Accuracy =" (/ (float errors) t)))))
 
+(defn update
+	"Returns an updated model by taking the last model, the next training 
+	 and applying the Pegasos update step"
+	[model example]
+	(let [lambda (:lambda model)
+		  t      (:step   model)
+		  w      (:w      model)
+		  errors (:errors model)
+		  error  (> (hinge-loss w example) 0)]
+		(do 
+			(report model 100)
+			{ :w      (if error (correct w example t lambda) w), 
+			  :lambda lambda, 
+			  :step   (inc t), 
+			  :errors (if error (inc errors) errors)} )))
+
+(defn train
+	"Returns a model trained from the initial model on the given examples"
+	[initial examples]
+	(reduce update initial examples))
+
+(defn main
+	"Trains a model from the examples and prints out its weights"
+	[]
+	(let [start 	{:lambda 0.0001, :step 1, :w {}, :errors 0} 
+		  examples 	(map parse (-> *in* BufferedReader. line-seq)) 
+		  model     (train start examples)]
+		(println (map #(str (key %) ":" (val %)) (:w model)))))
+;; End of Legacy Pegasos Algorythym for testing
+		
 
